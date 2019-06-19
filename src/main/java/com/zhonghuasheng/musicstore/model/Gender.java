abad9ecbb09d39e319fa3347b5fed0633a407975
luke.chen <luke.chen@augmentum.com>
2019-06-19 15:36:29 +0800

    Story #1 添加了创建用户的逻辑，并添加了单元测试

--- src/main/java/com/zhonghuasheng/musicstore/model/Gender.java
+++ src/main/java/com/zhonghuasheng/musicstore/model/Gender.java
@@ -2,5 +2,21 @@ package com.zhonghuasheng.musicstore.model;
 
 public enum Gender {
 
-    Male, Female, Unknow;
+    MALE("男"),
+    FEMALE("女"),
+    UNKNOW("未知");
+
+    private String value;
+
+    private Gender(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
