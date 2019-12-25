5ab55c20a4a756a2c9fdd93c91f1dc501e7054b9
luke.chen <luke.chen@augmentum.com>
2019-12-25 17:41:51 +0800

    feat(music): implements the music list functions #7

--- src/main/java/com/zhonghuasheng/musicstore/service/impl/MusicServiceImpl.java
+++ src/main/java/com/zhonghuasheng/musicstore/service/impl/MusicServiceImpl.java
@@ -3,10 +3,12 @@ package com.zhonghuasheng.musicstore.service.impl;
 import com.zhonghuasheng.musicstore.dao.MusicDAO;
 import com.zhonghuasheng.musicstore.dao.impl.MusicDAOImpl;
 import com.zhonghuasheng.musicstore.model.Music;
+import com.zhonghuasheng.musicstore.model.Pagination;
 import com.zhonghuasheng.musicstore.service.MusicService;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public class MusicServiceImpl implements MusicService {
@@ -21,5 +23,16 @@ public class MusicServiceImpl implements MusicService {
         music.setUuid(UUID.randomUUID());
 
         return musicDAO.create(music);
+    }
+
+    @Override
+    public List<Music> find(Pagination pagination) {
+        pagination.setKey("%" + pagination.getKey() + "%");
+        return musicDAO.find(pagination);
+    }
+
+    @Override
+    public int count() {
+        return musicDAO.count();
     }
 }
