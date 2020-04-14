3d4bcb2e48d6eebdf07f50d6d4f9b3bcba7c8e00
Hua Sheng <xiaoyong6906@126.com>
2020-04-14 21:43:17 +0800

    feat(artist): 实现了普通用户可以浏览艺人详情的功能

--- musicstore-jsp/src/main/java/com/zhonghuasheng/musicstore/action/artist/ViewAction.java
+++ musicstore-jsp/src/main/java/com/zhonghuasheng/musicstore/action/artist/ViewAction.java
@@ -1,20 +1,26 @@
 package com.zhonghuasheng.musicstore.action.artist;
 
+import com.zhonghuasheng.musicstore.model.Artist;
+import com.zhonghuasheng.musicstore.service.ArtistService;
+import com.zhonghuasheng.musicstore.service.impl.ArtistServiceImpl;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import javax.servlet.ServletException;
 
 @WebServlet(urlPatterns = "/artist/view")
 public class ViewAction extends HttpServlet {
 
+    private ArtistService artistService = new ArtistServiceImpl();
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String uuid = request.getParameter("uuid");
+        Artist artist = artistService.get(uuid);
+        request.setAttribute("artist", artist);
+        request.getRequestDispatcher("/WEB-INF/html/artist/view.jsp").forward(request, response);
     }
 }
