a6999eed5a0363a5824199b7879b8fe95af43b29
Luke Chen <xiaoyong6906@126.com>
2020-12-27 17:40:47 +0800

    feat(ms-init): 集成mybatis-plus dynamic datasource到项目中

--- musicstore-springboot-vue/ms-platform/ms-business/ms-user/src/main/java/com/zhonghuasheng/ms/UserApplication.java
+++ musicstore-springboot-vue/ms-platform/ms-business/ms-user/src/main/java/com/zhonghuasheng/ms/UserApplication.java
@@ -1,9 +1,10 @@
 package com.zhonghuasheng.ms;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 public class UserApplication {
 
     public static void main(String[] args) {
